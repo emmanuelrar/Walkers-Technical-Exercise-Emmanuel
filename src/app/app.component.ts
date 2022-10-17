6c7dfa422d631bd4e627f51a6ce5a32a68a3533d
Emmanuel R. Aguilar Rossi <emmanuelrar@gmail.com>
2022-10-17 20:27:45 -0300

    Step 1 – Basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent {
 
   title = 'Walker Test';
-  
+
   numberList: any[] = [];
 
   numberForm = new FormGroup({
@@ -21,6 +21,20 @@ export class AppComponent {
     console.log(this.numberForm.value);
 
     this.numberList.push(this.numberForm.value.number);
+  }
+
+  numberTypeClass = (d: number) => {
+    let cssClass = '';
+
+    if (d % 3) {
+      cssClass = 'walkers';
+    } else if (d % 5) {
+      cssClass = 'assessment';
+    } else if (d % 3 && d % 5) {
+      cssClass = 'both';
+    }
+
+    return cssClass;
   }
 
 }
